5a71c09c988e0ba1624920ce3fd03e4c3a271df8
matt <rtrmfs1@gmail.com>
2019-07-16 22:21:06 -0500

    javascript lesson 6

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,12 @@
+/*Learned that camelCase is the proper way to separate words when naming variables
+and functions*/
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
