55b726690a01930e821292e15e7e728a108a5f02
matt <rtrmfs1@gmail.com>
2019-07-16 23:27:35 -0500

    javascript lesson 21

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,9 @@
+//Learned how to store strings in variables
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Matt";
+var myLastName = "Simon";
