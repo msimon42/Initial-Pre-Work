ee50d5bbeb339d8e92e37203bf7fb807a16dac8b
matt <rtrmfs1@gmail.com>
2019-07-19 16:53:59 -0500

    javascript lesson 52

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,15 @@
+//Learned that fuctions do not necessarily need to return anything
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
