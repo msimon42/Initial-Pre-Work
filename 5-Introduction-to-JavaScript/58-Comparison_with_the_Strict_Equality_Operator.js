0693873c116bdd18e9dbca3b589779c0829e149b
matt <rtrmfs1@gmail.com>
2019-07-19 17:23:19 -0500

    javascript lesson 58

---
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -0,0 +1,11 @@
+//Learned the difference between == and ===
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(10);
