4ecdc1019682aa42c46aa54645407fceb0cafaa6
matt <rtrmfs1@gmail.com>
2019-07-18 22:13:40 -0500

    javascript lesson 46

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,15 @@
+//Learned how to create functions in JavaScript
+// Example
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
