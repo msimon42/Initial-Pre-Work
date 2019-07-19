29559c1fcf227c105efb9667a356fdfcdf161ce8
matt <rtrmfs1@gmail.com>
2019-07-18 21:22:41 -0500

    javascript lesson 35

---
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -0,0 +1,11 @@
+//Learned how to use a function to create a string
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "When the " + myAdjective + " " + myNoun + " saw the rabbit, he " + myVerb + " " + myAdverb + ".";;
+
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
