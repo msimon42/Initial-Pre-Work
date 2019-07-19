44ec44c3737e10c2b878a2cd8978ed98deda6321
matt <rtrmfs1@gmail.com>
2019-07-18 20:54:37 -0500

    javascript lesson 31

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,7 @@
+//Learned that strings in JavaScript are immutable when the variable it's stored in is reassigned
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
