c4ce3b344edd9a029c5816e493629509b1879c52
matt <rtrmfs1@gmail.com>
2019-07-18 22:49:52 -0500

    javascript lesson 50

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,14 @@
+//Created a local variable within a function that overrode a global variable
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
