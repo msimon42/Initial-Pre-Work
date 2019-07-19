ca92151abf67487b9264a61e139e435590e8e344
matt <rtrmfs1@gmail.com>
2019-07-18 21:29:56 -0500

    javascript lesson 37

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,6 @@
+//Learned how to form nested arrays in JavaScript
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["salad", 15],["chili", 42]];
