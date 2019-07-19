84563511e9ebb688278f823e1c35f5d45c99ddbf
matt <rtrmfs1@gmail.com>
2019-07-18 20:30:22 -0500

    javascript lesson 29

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,14 @@
+// Learned how to find out how many characters are in a string using .length
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
