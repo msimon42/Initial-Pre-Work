1c6973e1ec539154af2f674f3b85bda4fc8b37c8
matt <rtrmfs1@gmail.com>
2019-07-18 22:09:15 -0500

    javascript lesson 45

---
+++ 5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,7 @@
+//Created a list using information learned from previous lessons
+var myList = [["Chicken", 1],
+            ["Milk", 1],
+            ["Diced Tomatos", 2],
+            ["Bacon", 5],
+            ["Popcorn", 1]
+            ];
