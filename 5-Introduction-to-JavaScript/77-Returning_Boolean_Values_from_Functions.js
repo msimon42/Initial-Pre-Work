68238eef5589966d0ede397a2b3478c9f48227f2
matt <rtrmfs1@gmail.com>
2019-07-19 18:44:52 -0500

    javascript lesson 77

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,8 @@
+//Learned how to return booleans in functions instead of using If statements
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+}
+
+// Change these values to test
+isLess(10, 15);
