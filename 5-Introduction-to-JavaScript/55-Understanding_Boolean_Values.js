d321d02cb5f5516de51fd4b0956fed85cbf5d89f
matt <rtrmfs1@gmail.com>
2019-07-19 17:08:00 -0500

    javascript lesson 55

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+//Learned about the boolean data type in JavaScript
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
