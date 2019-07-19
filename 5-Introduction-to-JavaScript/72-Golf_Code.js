27e9497a2992df18a063989580f81eb6d8f61e1d
matt <rtrmfs1@gmail.com>
2019-07-19 18:16:37 -0500

    javascript lesson 72

---
+++ 5-Introduction-to-JavaScript/72-Golf_Code.js
@@ -0,0 +1,23 @@
+//Learned how to apply if-else statements to a real world example
+var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+function golfScore(par, strokes) {
+  if (strokes == 1) {
+    return "Hole-in-one!";
+  } else if (strokes <= par - 2) {
+    return "Eagle";
+  } else if (strokes == par - 1) {
+    return "Birdie";
+  } else if (strokes == par) {
+    return "Par";
+  } else if (strokes == par + 1) {
+    return "Bogey";
+  } else if (strokes == par + 2) {
+    return "Double Bogey";
+  } else if (strokes >= par + 3){
+    return "Go Home!";
+  }
+  // Only change code above this line
+}
+
+// Change these values to test
+golfScore(5, 4);
