cd1b7d0211f3ae8bd8743c2848e09df2a40e3c41
matt <rtrmfs1@gmail.com>
2019-07-18 22:18:41 -0500

    javascript lesson 47

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,13 @@
+//Learned how to create functions with arguments in JavaScript
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(num1, num2){
+  console.log(num1 + num2);
+}
+
+functionWithArgs(4, 2);
