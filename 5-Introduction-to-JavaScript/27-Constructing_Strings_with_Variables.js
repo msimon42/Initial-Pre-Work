7e50ae872d328b69aa0224765b2e2ac6e188e6ed
matt <rtrmfs1@gmail.com>
2019-07-18 20:13:58 -0500

    javascript lesson 27

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+//Learned how to combine strings with variables in JavaScript
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Matt";
+var myStr = "My name is " + myName + " and I am well!";
