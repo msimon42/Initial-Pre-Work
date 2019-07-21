5d444403e3886b28e382dc94babbacbd5cccca28
matt <rtrmfs1@gmail.com>
2019-07-20 23:39:24 -0500

    javascript lesson 82

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,12 @@
+//Learned how to select elements in an object using bracket notation
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
