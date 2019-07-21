cfc7bee68e9947013081631ab824eeb6a7936a3e
matt <rtrmfs1@gmail.com>
2019-07-21 17:23:13 -0500

    javascript lesson 95

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,15 @@
+//Learned how to increment by 2 instead of 1 in a for loop
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i < 10; i += 2){
+  myArray.push(i);
+}
