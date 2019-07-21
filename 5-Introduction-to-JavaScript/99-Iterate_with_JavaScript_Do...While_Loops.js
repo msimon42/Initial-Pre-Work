9787c7394f8c23eec4e60d6b95ade7fc1dff2ca3
matt <rtrmfs1@gmail.com>
2019-07-21 17:37:28 -0500

    javascript lesson 99

---
+++ 5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do...While_Loops.js
@@ -0,0 +1,11 @@
+//Learned the difference between While Loops and Do...While Loops
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+ do {
+  myArray.push(i);
+  i++;
+} while (i < 5)
