392ac8375e395949da54c74708573533e13dda4d
matt <rtrmfs1@gmail.com>
2019-07-21 00:25:43 -0500

    javascript lesson 89

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,27 @@
+//Learned about JSON and how to store objects in arrays
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+
+  {
+    "artist": "Trampled by Turtles",
+    "title": "Palomino",
+    "release_year": 2011,
+    "formats" : [
+      "mp3",
+      "CD"
+    ],
+    "gold": true
+  }
+
+
+];
