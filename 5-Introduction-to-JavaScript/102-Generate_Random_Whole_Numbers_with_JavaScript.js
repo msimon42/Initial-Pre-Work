683a0554150036dc832eff3f51a1e98b27e6e3f8
matt <rtrmfs1@gmail.com>
2019-07-21 18:19:28 -0500

    javascript lesson 102

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,9 @@
+//Learned how to generate random whole numbers using Math.random() and Math.floor()
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random()*10);
+}
