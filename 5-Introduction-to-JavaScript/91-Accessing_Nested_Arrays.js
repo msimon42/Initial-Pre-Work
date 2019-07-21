15fa7d4c9e9c10cc2cf1d45eb9717bbc78a247bf
matt <rtrmfs1@gmail.com>
2019-07-21 16:03:24 -0500

    javascript lesson 91

---
+++ 5-Introduction-to-JavaScript/91-Accessing_Nested_Arrays.js
@@ -0,0 +1,25 @@
+//Learned how to select an element of an array that is nested within an object
+// Setup
+// Setup
+var myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+// Only change code below this line
+
+var secondTree = myPlants[1].list[1]; // Change this line
