0ba2c554597cb2a6c2de07ce73e058ebcc0a3503
matt <rtrmfs1@gmail.com>
2019-07-20 23:46:41 -0500

    javascript lesson 83

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,12 @@
+//Learned how to store object properties in variables
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
