0c8377ddf8d0c58bb587600e304039fb219c6910
matt <rtrmfs1@gmail.com>
2019-07-20 23:34:58 -0500

    javascript lesson 80

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,17 @@
+//Learned how to build objects in JavaScript
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Louise",
+  "legs": 4,
+  "tails": 1,
+  "friends" : ["matt", "callie", "emma"]
+};
