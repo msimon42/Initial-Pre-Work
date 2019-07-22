a29a69d3b8d65edac6d49973f551b95b79fbd7c9
matt <rtrmfs1@gmail.com>
2019-07-21 22:40:40 -0500

    es6 challenge 1

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+//Learned the difference between using var and let when declaring variables
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
