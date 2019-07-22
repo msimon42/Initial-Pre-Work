32f17cb3abee9c4c7036febdf4c6019f36cfea56
matt <rtrmfs1@gmail.com>
2019-07-21 22:47:27 -0500

    es6 challenge 2

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,11 @@
+//Learned the variables declared using let within a function are not global
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
