ef1d9d6dd07f7e39b6ad7fc8a02ceaee0e058f1f
matt <rtrmfs1@gmail.com>
2019-07-23 02:23:37 -0500

    es6 challenge 14

---
+++ 6-Introduction-to-the-ES6-Challenges/14-Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,10 @@
+//Learned how to destructure arrays
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [b, a] = [a, b];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
