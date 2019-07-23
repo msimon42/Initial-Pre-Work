1ff6b79b0365a927f6d5fa0b8ffc9b3aa4ee7e0e
matt <rtrmfs1@gmail.com>
2019-07-23 02:06:17 -0500

    es6 challenge 6

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,8 @@
+//Learned how to copy an array using the spread operator
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
