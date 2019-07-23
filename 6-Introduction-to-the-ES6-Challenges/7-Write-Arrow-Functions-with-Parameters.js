7c4cc4223bc398be80a83a2f7f734f3163a12a8a
matt <rtrmfs1@gmail.com>
2019-07-23 01:40:43 -0500

    es6 challenge 7

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,7 @@
+//Learned how to add parameters to arrow functions
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
